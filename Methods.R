492e038650bc9726a0cc7d47f1132eeb1bee4cbc
Roy Lowrance <roy.lowrance@gmail.com>
2015-01-19 15:57:09 -0500

    new define all my methods

---
+++ Methods.R
@@ -0,0 +1,5 @@
+# define all S3 methods
+AsCharacter <- function(x, ...) UseMethod('AsCharacter')
+Command     <- function(x, ...) UseMethod('Command')
+Path        <- function(x, ...) UseMethod('Path')
+
