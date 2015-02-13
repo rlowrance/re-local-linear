a6d6c74cb3fd940c81cb8e55616f389d54a26db3
Roy Lowrance <roy.lowrance@gmail.com>
2015-02-13 16:39:40 -0500

    new

---
+++ test-all.sh
@@ -0,0 +1,11 @@
+python CvResult.py
+python directory.py
+python FoldResult.py
+python features.py
+python Logger.py
+python Maybe.py
+python np_equal_considering_nans.py
+python SCODE.py
+python TRNTP.py
+python round_to_n.py
+python year_month_day.py
