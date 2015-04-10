44e04556f369dd101c5a72e4a5a70313308c4f08
Roy Lowrance <roy.lowrance@gmail.com>
2015-04-10 07:14:06 -0400

    conform to new API for chart_02_04_05

--- chart-02.py
+++ chart-02.py
@@ -1,18 +1,33 @@
-# create files (selective on how invoked)
-# WORKING/chart-02.makefile
-# WORKING/chart-02.data
-# WORKING/chart-02.txt-mean-root-mean-squared-errors.txt
-# WORKING/chart-02.txt-median-root-median-squared-errors.txt
+# create files for chart-02
+#  python chart-02.py makefile
+#   src/chart-02.makefile
+#  python chart-02.py data
+#   data/working/chart-02.data
+#  python chart-02.py txt SUFFIX
+#   data/working/chart-02-SUFFIX.txt
+# where
+#  SUFFIX in
+#   mean-root-mean-squared-errors  (NOT RUN)
+#   median-root-median-squared-errors
 
 import sys
-import chart_02_04
+from chart_02_04_05 import chart
 
 
 def main():
+    model = 'ols'
+    predictors = ['act', 'actlog', 'ct', 'ctlog']
+    responses = ['price', 'logprice']
+    year = '2008'
+
     args = sys.argv
-    chart_02_04.chart_02_04(chart_id='02',
-                            suffix=args[1],
-                            specific=args[2] if len(args) == 3 else None)
+    chart(model=model,
+          predictors=predictors,
+          responses=responses,
+          year=year,
+          chart_id='02',
+          suffix=args[1],
+          specific=args[2] if len(args) == 3 else None)
 
 if __name__ == '__main__':
     main()
