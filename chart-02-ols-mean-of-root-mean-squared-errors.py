eb71d70831ce45d4ca7604b188a4be93f2b9f972
Roy Lowrance <roy.lowrance@gmail.com>
2015-04-14 07:43:39 -0400

    new

---
+++ chart-02-ols-mean-of-root-mean-squared-errors.py
@@ -0,0 +1,38 @@
+# create files for chart-02-ols-mean-of-root-mean-squared-errors
+# with these choices
+#  metric     in mean-root-mean-squared-errors
+#  model      in ols
+#  ndays      in 30 60 ... 360
+#  predictors in act actlog ct ctlog
+#  responses  in price logprice
+#  usetax     in yes no
+#  year       in 2008
+# invocations and files created
+#  python chart-02X.py makefile -> src/chart-02X.makefile
+#  python chart-02X.py data     -> data/working/chart-02X.data
+#  python chart-02X.py txt      -> data/working/chart-02X.txt
+#  python chart-02X.py txtY     -> data/working/chart-02X-Y.txt
+
+import sys
+
+
+from Bunch import Bunch
+from chart_02_template import chart
+
+
+def main():
+    specs = Bunch(metric='mean-of-root-mean-squared-errors',
+                  title='Mean of Root Mean Squared Errors',
+                  model='ols',
+                  training_periods=['30', '60', '90', '120', '150', '180',
+                                    '210', '240', '270', '300', '330', '360'],
+                  feature_sets=['act', 'actlog', 'ct', 'ctlog'],
+                  responses=['price', 'logprice'],
+                  usetax=['yes', 'no'],
+                  year='2008')
+    chart(specs=specs,
+          argv=sys.argv)
+
+
+if __name__ == '__main__':
+    main()
