a37705a2ca3b0561d1f6f9e208cd2595364d79b7
Roy Lowrance <roy.lowrance@gmail.com>
2015-05-19 08:49:33 -0400

    refactor: move Test class to outer scope

--- FoldResult.py
+++ FoldResult.py
@@ -2,6 +2,7 @@
 import datetime
 import numpy as np
 import pdb
+import unittest
 
 # local imports
 import Maybe
@@ -74,134 +75,106 @@ class FoldResult(object):
             return Maybe.Maybe(errors_without_nans)
 
 
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.verbose = False
+        self.group1_actuals = np.array([1, 2, np.nan, 4])
+        self.group1_estimates = np.array([2, 3, 4, np.nan])
+
+        self.group2_actuals = np.array([10])
+        self.group2_estimates = np.array([20])
+
+        # build the FoldResult object
+        fr = FoldResult()
+        fr.extend(self.group1_actuals,
+                  self.group1_estimates)
+        fr.extend(self.group2_actuals,
+                  self.group2_estimates)
+        self.fr = fr
+
+    def test_fitted(self):
+        # test methods .save_fitted() and .get_fitted()
+        fr = self.fr
+        fitted1 = ['a', 'b']
+        date1 = datetime.date(2015, 05, 18)
+        fr.save_fitted(date1, fitted1)
+        fitted2 = ['aa', 'bb']
+        date2 = datetime.date(2015, 05, 19)
+        fr.save_fitted(date2, fitted2)
+        fitted = fr.get_fitted()  # return a dict
+        self.assertEqual(len(fitted), 2)
+
+    def test_predictor_names(self):
+        # test methods .save_preictor_names() and .get_predictor_names()
+        fr = self.fr
+        names1 = ['a', 'b']
+        date1 = datetime.date(2015, 05, 18)
+        fr.save_predictor_names(date1, names1)
+        names2 = ['aa']
+        date2 = datetime.date(2015, 05, 19)
+        fr.save_predictor_names(date2, names2)
+        predictor_names = fr.get_predictor_names()
+        self.assertEqual(len(predictor_names), 2)
+
+    def test_num_test(self):
+        fr = self.fr
+        fr.save_num_test(datetime.date(2015, 05, 18), 10)
+        fr.save_num_test(datetime.date(2015, 05, 19), 20)
+        num_tests = fr.get_num_test()
+        self.assertEqual(len(num_tests), 2)
+
+    def test_num_train(self):
+        fr = self.fr
+        fr.save_num_train(datetime.date(2015, 05, 18), 10)
+        fr.save_num_train(datetime.date(2015, 05, 19), 20)
+        num_trains = fr.get_num_train()
+        self.assertEqual(len(num_trains), 2)
+
+    def test_has_elements(self):
+        me = self.fr.maybe_errors()
+        self.assertTrue(me.has_value)
+        value = me.value
+        expected_errors = np.array([-1, -1, -10])
+        for i in xrange(3):
+            self.assertEqual(value[i], expected_errors[i])
+
+    def test_has_no_elements(self):
+        fr = FoldResult()
+        fr.extend(np.array([1, np.nan]),
+                  np.array([np.nan, 2]))
+        me = fr.maybe_errors()
+        self.assertTrue(not me.has_value)
+
+    def test_extend_one(self):
+        a = np.array([1, 2, 3])
+        b = np.array([4, 5, 6])
+        fr = FoldResult()
+        fr.extend(a, b)
+        if self.verbose:
+            print fr.actuals
+            print fr.estimates
+            self.assertEqual(fr.actuals.size, 3)
+            self.assertEqual(fr.actuals[1], 2)
+
+    def test_extend_two(self):
+        a = np.array([1, 2, 3])
+        b = np.array([4, 5, 6])
+        fr = FoldResult()
+        fr.extend(a, b)
+        fr.extend(np.array([10, 20]),
+                  np.array([40, 50]))
+        if self.verbose:
+            print fr.actuals
+            print fr.estimates
+            self.assertEqual(fr.estimates.size, 5)
+            self.assertEqual(fr.estimates[4], 50)
+
+
 if __name__ == '__main__':
     import unittest
 
     if False:
         pdb.set_trace()  # avoid warning
 
-    class Test(unittest.TestCase):
-
-        def setUp(self):
-            self.verbose = False
-            self.group1_actuals = np.array([1, 2, np.nan, 4])
-            self.group1_estimates = np.array([2, 3, 4, np.nan])
-
-            self.group2_actuals = np.array([10])
-            self.group2_estimates = np.array([20])
-
-            # build the FoldResult object
-            fr = FoldResult()
-            fr.extend(self.group1_actuals,
-                      self.group1_estimates)
-            fr.extend(self.group2_actuals,
-                      self.group2_estimates)
-            self.fr = fr
-
-        def test_fitted(self):
-            # test methods .save_fitted() and .get_fitted()
-            fr = self.fr
-            fitted1 = ['a', 'b']
-            date1 = datetime.date(2015, 05, 18)
-            fr.save_fitted(date1, fitted1)
-            fitted2 = ['aa', 'bb']
-            date2 = datetime.date(2015, 05, 19)
-            fr.save_fitted(date2, fitted2)
-            fitted = fr.get_fitted()  # return a dict
-            self.assertEqual(len(fitted), 2)
-
-        def test_predictor_names(self):
-            # test methods .save_preictor_names() and .get_predictor_names()
-            fr = self.fr
-            names1 = ['a', 'b']
-            date1 = datetime.date(2015, 05, 18)
-            fr.save_predictor_names(date1, names1)
-            names2 = ['aa']
-            date2 = datetime.date(2015, 05, 19)
-            fr.save_predictor_names(date2, names2)
-            predictor_names = fr.get_predictor_names()
-            self.assertEqual(len(predictor_names), 2)
-
-        def test_num_test(self):
-            fr = self.fr
-            fr.save_num_test(datetime.date(2015, 05, 18), 10)
-            fr.save_num_test(datetime.date(2015, 05, 19), 20)
-            num_tests = fr.get_num_test()
-            self.assertEqual(len(num_tests), 2)
-
-        def test_num_train(self):
-            fr = self.fr
-            fr.save_num_train(datetime.date(2015, 05, 18), 10)
-            fr.save_num_train(datetime.date(2015, 05, 19), 20)
-            num_trains = fr.get_num_train()
-            self.assertEqual(len(num_trains), 2)
-
-        def test_has_elements(self):
-            me = self.fr.maybe_errors()
-            self.assertTrue(me.has_value)
-            value = me.value
-            expected_errors = np.array([-1, -1, -10])
-            for i in xrange(3):
-                self.assertEqual(value[i], expected_errors[i])
-
-        def test_has_no_elements(self):
-            fr = FoldResult()
-            fr.extend(np.array([1, np.nan]),
-                      np.array([np.nan, 2]))
-            me = fr.maybe_errors()
-            self.assertTrue(not me.has_value)
-
-        def test_extend_one(self):
-            a = np.array([1, 2, 3])
-            b = np.array([4, 5, 6])
-            fr = FoldResult()
-            fr.extend(a, b)
-            if self.verbose:
-                print fr.actuals
-                print fr.estimates
-            self.assertEqual(fr.actuals.size, 3)
-            self.assertEqual(fr.actuals[1], 2)
-
-        def test_extend_two(self):
-            a = np.array([1, 2, 3])
-            b = np.array([4, 5, 6])
-            fr = FoldResult()
-            fr.extend(a, b)
-            fr.extend(np.array([10, 20]),
-                      np.array([40, 50]))
-            if self.verbose:
-                print fr.actuals
-                print fr.estimates
-            self.assertEqual(fr.estimates.size, 5)
-            self.assertEqual(fr.estimates[4], 50)
-
-#        def test_mean_exists(self):
-#            fr = FoldResult()
-#            fr.extend(np.array([1, 2, np.nan, 10, 20]),
-#                      np.array([2, 3, 4,      20, np.nan]))
-#            value = fr.mean_error_ignore_nans()
-#            expected = -(1 + 1 + 10) / 3.0
-#            self.assertTrue(value.has_value)
-#            self.assertAlmostEqual(value.value, expected)
-
-#        def test_mean_doesnt_exist(self):
-#            actuals = np.array([np.nan, 2])
-#            estimates = np.array([1, np.nan])
-#            fr = FoldResult()
-#            fr.extend(actuals, estimates)
-#            mean_error = fr.mean_error_ignore_nans()
-#            self.assertTrue(not mean_error.has_value)
-#
-#        def test_median_exists(self):
-#            value = self.fr.median_error_ignore_nans()
-#            expected = -1
-#            self.assertTrue(value.has_value)
-#            self.assertAlmostEqual(value.value, expected)
-#
-#        def test_median_doesnt_exists(self):
-#            fr = FoldResult()
-#            fr.extend(np.array([]), np.array([]))
-#            value = fr.median_error_ignore_nans()
-#            self.assertTrue(not value.has_value)
-#
     unittest.main()
