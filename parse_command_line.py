baa69e098699c4791375bf8556e8783d9af540f7
Roy Lowrance <roy.lowrance@gmail.com>
2015-08-26 20:49:08 -0400

    get_arg retuns list of values until next tag
    Added unit tests.

--- parse_command_line.py
+++ parse_command_line.py
@@ -2,14 +2,27 @@
 
 
 def get_arg(argv, tag):
-    # return value past the tag or None
+    '''return value or list of values past the tag, or None
+
+    --tag v1 --tag        ==> return v1 as a string
+    --tag v1 v2 v3 --tag  ==> return v1 v2 v3 as list of string
+    --tag v1 v2 v3        ==> return v1 v2 v3 as list of string
+    --tag --tag           ==> return []
+                          ==> return None
+    '''
     for i in xrange(len(argv)):
         if argv[i] == tag:
-            return argv[i + 1]
+            result = []
+            i += 1
+            while i < len(argv) and argv[i][:2] != '--':
+                result.append(argv[i])
+                i += 1
+            return result[0] if len(result) == 1 else result
     return None
 
 
 def has_arg(argv, tag):
+    'return True iff argv contains the tag'
     for i in xrange(len(argv)):
         if argv[i] == tag:
             return True
@@ -19,3 +32,25 @@ def has_arg(argv, tag):
 def default(argv, tag, default_value):
     actual = get_arg(argv, tag)
     return default_value if actual is None else actual
+
+
+if __name__ == '__main__':
+    def a(s):
+        'convert string to list of strings'
+        return s.split(' ')
+
+    # test get_arg
+    assert get_arg(a('--x a b c'), '--x') == ['a', 'b', 'c']
+    assert get_arg(a('--x a'), '--x') == 'a'
+    assert get_arg(a('--x a b --x'), '--x') == ['a', 'b']
+    assert get_arg(a('--x --y'), '--y') == []
+    assert get_arg(a('--y'), '--x') is None
+
+    # test has_arg
+    assert has_arg(a('--x'), '--x')
+    assert not has_arg(a('--x'), '--y')
+
+    # default
+    assert default(a('--x'), 'x', 'def') == 'def'
+
+    print 'ok'
